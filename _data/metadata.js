db1a1ee284414cf4fe416b6e8e938b1c5dc8c1f8
Andrew Shell <andrew@andrewshell.org>
2024-09-02 10:22:54 -0500

    Only include analytics in prod

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,6 +3,7 @@ module.exports = {
 	url: "https://blog.andrewshell.org/",
 	language: "en",
 	description: "Strategies for thinking, learning, and productivity.",
+  production: process.env.ELEVENTY_RUN_MODE === 'build',
 	author: {
 		name: "Andrew Shell",
 		givenName: "Andrew",
