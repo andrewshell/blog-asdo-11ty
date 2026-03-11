b2e0a4372437c9d6c5e853c15db47ebd0738ec0a
Andrew Shell <andrew@andrewshell.org>
2026-03-11 07:36:39 -0500

    moving wordpress theme to own repo

--- wordpress/404.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-
-<h1>Content not found.</h1>
-<p>Go <a href="<?php echo esc_url(home_url('/')); ?>">home</a> or <a href="<?php echo esc_url(home_url('/search/')); ?>">search</a>.</p>
-
-<?php get_footer(); ?>
